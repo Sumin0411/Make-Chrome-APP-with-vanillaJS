155f1be246ba604eca0e9a4d980d57cc712dd575
Sumin0411 <kimsumin411@gmail.com>
2024-03-14 22:56:40 +0900

    Basic Data types and Variables / March 14th

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("hi"); 
+// alert("hi"); 
+const a = 6;
+const b = 3;
+const myName = "Sumin";
+
+console.log("lalala");
+console.log(a - 2);
+console.log(a * 2);
+console.log(b * 2);
+console.log("Hello " + myName);
