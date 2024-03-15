a891f4c5f79bb8a15c9c0d0edb3c43a773b190b8
Sumin0411 <kimsumin411@gmail.com>
2024-03-15 16:51:47 +0900

    Const and let | March 15th

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // alert("hi"); 
 const a = 6;
 const b = 3;
-const myName = "Sumin";
+// const myName = "Sumin";
+let myName = "Sumin";
+
 
 console.log("lalala");
 console.log(a - 2);
 console.log(a * 2);
 console.log(b * 2);
 console.log("Hello " + myName);
+
+myName = "Kim Sumin";
+console.log("My full name is " + myName);
