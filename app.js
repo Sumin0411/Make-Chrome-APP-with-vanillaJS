8013b72d8924dc6ce7810080c242615985f48ccb
Sumin0411 <kimsumin411@gmail.com>
2024-03-27 18:04:43 +0900

    function | Mar 27th

--- app.js
+++ app.js
@@ -14,6 +14,8 @@
 // myName = "Kim Sumin";
 // console.log("My full name is " + myName);
 
+// 2.5 ~ 2.6 arrays and objects
+
 //  const mon = "mon";
 //  const tue = "tue";
 //  const wed = "wed";
@@ -31,13 +33,44 @@
 
 //설명이 필요하지 않은 데이터 리스트들은 array로, 설명이 필요한 정보가 담긴 데이터 리스트들은 object로!
 
+// const player = {
+//     name : "sumin",
+//     points : 10,
+//     fat: true,
+// };
+// console.log(player);
+// player.lastName = "potato";
+// player.points = 15;
+// console.log(player);
+
+
+// 2.7 Function part 1,2
+
+// function sayHello(nameOfPerson, age){
+//     console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+// }
+
+// sayHello("sumin", 10);
+// sayHello("soo", 20);
+// sayHello("lyn", 30);
+
+// function plus(firstNumber, secondNumber){
+//     console.log(firstNumber + secondNumber);
+// }
+
+// function divide(a, b){
+//     console.log(a / b);
+// }
+// plus(8, 29);
+// divide(98,20);
+
 const player = {
     name : "sumin",
-    points : 10,
-    fat: true,
+    sayHello : function(otherPersonsName){
+        console.log("Hello " + otherPersonsName + " nice to meet you");
+    },
 };
-console.log(player);
-player.lastName = "potato";
-player.points = 15;
-console.log(player);
 
+console.log(player.name);
+player.sayHello("lyn");
+player.sayHello("soo");
