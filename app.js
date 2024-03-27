0a33b124cf6bf1221e4118ab0ef25f8b1b7a3776
Sumin0411 <kimsumin411@gmail.com>
2024-03-27 14:54:41 +0900

    Array | Mar 27th

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-// alert("hi"); 
-const a = 6;
-const b = 3;
-// const myName = "Sumin";
-let myName = "Sumin";
+// // alert("hi"); 
+// const a = 6;
+// const b = 3;
+// // const myName = "Sumin";
+// let myName = "Sumin";
 
 
-console.log("lalala");
-console.log(a - 2);
-console.log(a * 2);
-console.log(b * 2);
-console.log("Hello " + myName);
+// console.log("lalala");
+// console.log(a - 2);
+// console.log(a * 2);
+// console.log(b * 2);
+// console.log("Hello " + myName);
 
-myName = "Kim Sumin";
-console.log("My full name is " + myName);
+// myName = "Kim Sumin";
+// console.log("My full name is " + myName);
+
+//  const mon = "mon";
+//  const tue = "tue";
+//  const wed = "wed";
+//  const thu = "thu";
+//  const fri = "fri";
+//  const sat = "sat";
+//  const sun = "sun";
+
+ const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
+ //get item from array
+ console.log(daysOfWeek[0]);
+// add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
