171135c29da72f307f9e871c0858e8436b4bf444
Sumin0411 <kimsumin411@gmail.com>
2024-03-27 15:55:40 +0900

    object | Mar 27th

--- app.js
+++ app.js
@@ -22,9 +22,22 @@
 //  const sat = "sat";
 //  const sun = "sun";
 
- const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
- //get item from array
- console.log(daysOfWeek[0]);
-// add one more day to the array
-daysOfWeek.push("sun");
-console.log(daysOfWeek);
+//  const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
+//  //get item from array
+//  console.log(daysOfWeek[0]);
+// // add one more day to the array
+// daysOfWeek.push("sun");
+// console.log(daysOfWeek);
+
+//설명이 필요하지 않은 데이터 리스트들은 array로, 설명이 필요한 정보가 담긴 데이터 리스트들은 object로!
+
+const player = {
+    name : "sumin",
+    points : 10,
+    fat: true,
+};
+console.log(player);
+player.lastName = "potato";
+player.points = 15;
+console.log(player);
+
